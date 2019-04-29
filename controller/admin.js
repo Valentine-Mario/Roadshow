68d337c4b1a1f1aa8630d5e3a91a7b91d8a9093e
valentine-mario <valentine13400@gmail.com>
2019-04-29 16:58:15 +0100

    added function for admin to delete user account

--- controller/admin.js
+++ controller/admin.js
@@ -45,3 +45,116 @@ exports.getUserId=(req, res)=>{
         console.log(e)
     }
 }
+
+exports.editUser=(req, res)=>{
+    var data={
+        name:req.body.name,
+        email:req.body.email,
+    }
+    var id={_id:req.params.id}
+    try{
+        jwt.verify(req.token, "golden_little_kids", (err, decoded_user)=>{
+            adminModel.findById(decoded_user.user, (err, admin)=>{
+                if(admin.access <1){
+                    res.json({code:"01", message:"only admin authorised to view this route"})
+                }else{
+                    adminModel.findByIdAndUpdate(id, data, (err)=>{
+                        if(err)res.json({code:"01", err:err, message:"error updating user"})
+                        res.json({code:"00", message:"update successful"})
+                    })
+                }
+            })
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+
+exports.approveUser=(req, res)=>{
+    var id={_id:req.params.id}
+    var data={
+        verified:true
+    }
+    try{
+        jwt.verify(req.token, "golden_little_kids", (err, decoded_user)=>{
+            adminModel.findById(decoded_user.user, (err, admin)=>{
+                if(admin.access< 1){
+                    res.json({code:"01", message:"only admin authorised to view this route"})
+                }else{
+                    adminModel.findByIdAndUpdate(id, data, (err)=>{
+                        if(err)res.json({code:"01", err:err, message:"error approving user"})
+                        res.json({code:"00", message:"approve successful"})
+                    })
+                }
+            })
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+
+exports.makeAdmin=(req, res)=>{
+    var id={_id:req.params.id}
+    var data={
+        access:1
+    }
+    try{
+        jwt.verify(req.token, "golden_little_kids", (err, decoded_user)=>{
+            adminModel.findById(decoded_user.user, (err, admin)=>{
+                if(admin.access<1){
+                    res.json({code:"01", message:"only admin authorised to view this route"})
+                }else{
+                    adminModel.findByIdAndUpdate(id, data, (err)=>{
+                        if(err)res.json({code:"01", err:err, message:`error making user admin`})
+                        res.json({code:"00", message:"user made admin successfully"})
+                    })
+                }
+            })
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+
+exports.removeAdmin=(req, res)=>{
+    var id={_id:req.params.id}
+    var data={
+        access:0
+    }
+    try{
+        jwt.verify(req.token, "golden_little_kids", (err, decoded_user)=>{
+            adminModel.findById(decoded_user.user, (err, admin)=>{
+                if(admin.access<1){
+                    res.json({code:"01", message:"only admin authorised to view this route"})
+                }else{
+                    adminModel.findByIdAndUpdate(id, data, (err)=>{
+                        if(err)res.json({code:"01", err:err, message:`error removing user as admin`})
+                        res.json({code:"00", message:"user removed as admin successfully"})
+                    })
+                }
+            })
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+
+exports.deleteUser=(req, res)=>{
+    var id={_id:req.params.id}
+    try{
+        jwt.verify(req.token, "golden_little_kids", (err, decoded_user)=>{
+            adminModel.findById(decoded_user.user, (err, admin)=>{
+                if(admin.access<1){
+                    res.json({code:"01", message:"only admin authorised to view this route"})
+                }else{
+                    adminModel.findByIdAndRemove(id, (err)=>{
+                        if(err)res.json({code:"01", message:"error deleting user details"})
+                        res.json({code:"00", message:"user deleted successfully"})
+                    })
+                }
+            })
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
