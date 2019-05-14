5553fbacccfc53a6b6c06f0dc67d6aed42a22446
valentine-mario <valentine13400@gmail.com>
2019-05-14 12:44:50 +0100

    added function to delete and get hotel reviews

--- controller/hotel-review.js
+++ controller/hotel-review.js
@@ -33,11 +33,69 @@ exports.addReview=(req, res)=>{
                         }
                         hotelModel.findByIdAndUpdate(id, update_data, (err)=>{
                             if(err)res.json({code:"01", message:"error adding reviews"})
-                            res.json({code:"00", message:"review added successfully"})
+                            res.json({code:"00", message:review})
                         })
                     })
                 })
             }
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+
+exports.removeReview=(req, res)=>{
+    var id={_id:req.params.id}
+    var hotel_id={_id:req.params.hotel_id}
+    try{
+        jwt.verify(req.token, "golden_little_kids", (err, decoded_user)=>{
+            reviewModel.findById(id, (err, review)=>{
+                if(JSON.stringify(review.user)!==JSON.stringify(decoded_user.user)){
+                    res.json({code:"01", message:"unauthorised to delete this review"})
+                }else{
+                    // console.log(JSON.stringify(hotel_id._id), JSON.stringify(review.hotel))
+                    if(JSON.stringify(hotel_id._id)==JSON.stringify(review.hotel)){
+                        hotelModel.findById(hotel_id, (err, hotel)=>{
+                            var index=review.rating
+                            hotel.rates.splice(index, 1)
+                            hotel.save()
+                            var sum=hotel.rates.reduce(function(a,b){
+                                return a+b
+                            })
+                            mean=parseInt(sum)/parseInt(hotel.rates.length)
+                            meanVal=mean.toFixed(2)
+                            var update_data={
+                                rate_value:meanVal
+                            }
+                            hotelModel.findByIdAndUpdate(hotel_id, update_data, (err)=>{
+                                if(err)res.json({code:"01", message:"error deleting reviews"})
+                                res.json({code:"00", message:"review deleted successfully"})
+                            })
+                        })
+                    }else{
+                        res.json({code:"01", message:"hotel not found"})
+                    }
+                }
+            })
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+
+exports.getReviews=(req, res)=>{
+    var id={_id:req.params.id}
+    var {page, limit}= req.query;
+   var options={
+       page:parseInt(page, 10) || 1,
+       limit:parseInt(limit, 10) || 10,
+       sort:{'_id':-1},
+       populate:'user'
+}
+    try{
+        reviewModel.paginate({hotel:id._id}, options, (err, value)=>{
+            if(err)res.json({code:"01", message:"error getting review"})
+            res.json({code:"00", message:value})
         })
     }catch(e){
         console.log(e)
--- routes/hotel-review.js
+++ routes/hotel-review.js
@@ -4,5 +4,7 @@ const verification= require('../controller/verification')
 var hotel_reviewController=require('../controller/hotel-review')
 
 router.post('/add/:id', verification.verifyToken, hotel_reviewController.addReview)
+router.get('/remove/:id/:hotel_id', verification.verifyToken, hotel_reviewController.removeReview)
+router.get('/get/:id', hotel_reviewController.getReviews)
 
 module.exports = router;
