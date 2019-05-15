e336ad04c10f3176770c54dee763e6825bf3b0f2
valentine-mario <valentine13400@gmail.com>
2019-05-15 16:01:56 +0100

    added schema for flights

---
+++ models/flight.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var mongoosePaginate = require('mongoose-paginate');
+var schema= new mongoose.Schema({
+    destination_from:String,
+    destination_to:String,
+    departure_Date:Date,
+    arrival_date:Date
+    departure_time:String,
+    arrival_time:String,
+    airline:{type:mongoose.Schema.Types.ObjectId, ref:'airline'},
+    class:String
+})
+schema.plugin(mongoosePaginate);
+module.exports= mongoose.model('flight', schema);
