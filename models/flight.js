79860f957aec4fda7d428d4ff02815837a1f892e
valentine-mario <valentine13400@gmail.com>
2019-06-01 11:24:06 +0100

    chenged data type

--- models/flight.js
+++ models/flight.js
@@ -3,8 +3,8 @@ var mongoosePaginate = require('mongoose-paginate');
 var schema= new mongoose.Schema({
     destination_from:String,
     destination_to:String,
-    departure_date:Date,
-    arrival_date:Date,
+    departure_date:String,
+    arrival_date:String,
     departure_time:String,
     arrival_time:String,
     airline:{type:mongoose.Schema.Types.ObjectId, ref:'airline'},
