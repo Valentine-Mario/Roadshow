74f9ded44beaa491406fd5c44ad41c5b6b15f0f6
valentine-mario <valentine13400@gmail.com>
2019-08-19 15:38:43 +0100

    added function to edit business profile and delete business acc

--- controller/business.js
+++ controller/business.js
@@ -97,5 +97,75 @@ class business{
             console.log(e)
         }
     }
+
+
+        editProfile(req, res){
+            var data={
+                name:req.body.name,
+                email:req.body.email,
+                description:req.body.description  
+            }
+            try{
+                auth.verifyBusinessToken(req.token).then(business=>{
+                    business_model.findByIdAndUpdate(business._id, data, (err)=>{
+                        if(err){
+                            if (err.name === 'MongoError' && err.code === 11000) {
+                                res.status(201).json({code:"01", message:"email already exist"})
+                              }
+                           } else{
+                            res.status(200).json({code:"00", message:"update successful"});
+                           }  
+                    })
+                })
+            }catch(e){
+                console.log(e)
+            }
+        }
+
+
+        updatePassword(req, res){
+            var old_password= req.body.old_password
+            var data={
+                password:req.body.password
+            }
+            try{
+                auth.verifyBusinessToken(req.token).then(business=>{
+                    hasher.compare_password(old_password, business.password).then(value=>{
+                        if(!value){
+                            res.status(201).json({message:"wrong old password"})
+                        }else{
+                            if(data.password.length<6){
+                                res.status(201).json({code:"01", message:"password should be 6 or more characters"})
+                            }else{
+                                hasher.hash_password(data.password).then(hashed=>{
+                                    data.password = hashed;
+                                    business_model.findOneAndUpdate(business._id, data, function(err){
+                                        if(err) res.status(501).json({code:"01", err:err, message:"error, could not update password"})
+                                        res.status(200).json({code:"00", message:"password changed successfully."})
+                                    })
+                                })
+                            }
+                            
+                        }
+                    })
+                    })
+            }catch(e){
+                console.log(e)
+            }
+        }
+    
+
+        deleteAcc(req, res){
+            try{
+                auth.verifyBusinessToken(req.token).then(business=>{
+                    business_model.findOneAndDelete(business._id, (err)=>{
+                        if(err) res.status(501).json({code:"01", message:err})
+                        res.status(200).json({code:"00", message:"account deleted successfully"})
+                    })
+                })
+            }catch(e){
+                console.log(e)
+            }
+        }
 }
 module.exports=new business();
--- routes/business.js
+++ routes/business.js
@@ -7,4 +7,7 @@ router.post('/add', business_controller.add_buisness)
 router.get('/approve', verification.verifyMail, business_controller.verify_email)
 router.post('/login', business_controller.login)
 router.get('/get', verification.verifyToken, business_controller.getProfile)
+router.post('/update', verification.verifyToken, business_controller.editProfile)
+router.post('/updatepassword', verification.verifyToken, business_controller.updatePassword)
+router.get('/delete', verification.verifyToken, business_controller.deleteAcc)
 module.exports = router;
