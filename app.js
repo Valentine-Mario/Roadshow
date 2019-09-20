782d5b22333ee33db599f760aa504e765ff98db1
valentine-mario <valentine13400@gmail.com>
2019-09-20 08:40:21 +0100

    heroku update

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ var timeout = require('express-timeout-handler');
 var app = express();
 
 app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true}));
 app.use(passport.initialize());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
